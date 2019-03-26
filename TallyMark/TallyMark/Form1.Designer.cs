6fa29479f0b939207d89f810c99a2b63677aba26
MoonMan42 <34987599+MoonMan42@users.noreply.github.com>
2019-03-26 12:44:58 -0400

    Add files via upload
    Added files

---
+++ TallyMark/TallyMark/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace TallyMark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.MinusButton = new System.Windows.Forms.Button();
+            this.numberLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NameBox
+            // 
+            this.NameBox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameBox.Location = new System.Drawing.Point(13, 13);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(318, 27);
+            this.NameBox.TabIndex = 2;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddButton.Location = new System.Drawing.Point(118, 46);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 3;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // MinusButton
+            // 
+            this.MinusButton.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinusButton.Location = new System.Drawing.Point(118, 138);
+            this.MinusButton.Name = "MinusButton";
+            this.MinusButton.Size = new System.Drawing.Size(75, 23);
+            this.MinusButton.TabIndex = 4;
+            this.MinusButton.Text = "Minus";
+            this.MinusButton.UseVisualStyleBackColor = true;
+            this.MinusButton.Click += new System.EventHandler(this.MinusButton_Click);
+            // 
+            // numberLabel
+            // 
+            this.numberLabel.AutoSize = true;
+            this.numberLabel.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberLabel.Location = new System.Drawing.Point(131, 91);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(49, 19);
+            this.numberLabel.TabIndex = 5;
+            this.numberLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(343, 185);
+            this.Controls.Add(this.numberLabel);
+            this.Controls.Add(this.MinusButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.NameBox);
+            this.Name = "Form1";
+            this.Text = "TallyMark";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button MinusButton;
+        private System.Windows.Forms.Label numberLabel;
+    }
+}
+
---
+++ TallyMark/TallyMark/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TallyMark
+{
+    public partial class Form1 : Form
+    {
+        private int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            numberLabel.Text = $"{count}";
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            count += 1;
+            numberLabel.Text = $"{count}";
+        }
+
+        private void MinusButton_Click(object sender, EventArgs e)
+        {
+            if (count > 0)
+            {
+                count -= 1;
+                numberLabel.Text = $"{count}";
+            }
+            
+        }
+    }
+}
